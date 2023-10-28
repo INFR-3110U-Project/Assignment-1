e63d87bf87c5bbe5ae5cadf0131905e5fdb08410
Jayce Lovell <19291090+JayceLovell@users.noreply.github.com>
2023-10-28 17:07:11 -0400

    Update PickupFactory.cs

--- Assets/FPS/Scripts/Gameplay/PickupFactory.cs
+++ Assets/FPS/Scripts/Gameplay/PickupFactory.cs
@@ -29,8 +29,26 @@ public class PickupFactory : MonoBehaviour
             return pickup;
         }
 
-        // If the type is not found, return a default prefab or handle it as you prefer
-        return Instantiate(defaultPrefab);
+        // If the type is not found, return a default prefab in front of the player
+        if (defaultPrefab != null)
+        {
+            // Access the player's position and forward direction
+            Transform playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+            Vector3 playerPosition = playerTransform.position;
+            Vector3 playerForward = playerTransform.forward;
+
+            // Calculate the position in front of the player
+            Vector3 spawnPosition = playerPosition + playerForward * 2.0f; // Adjust the distance as needed
+
+            // Instantiate the default prefab at the calculated position
+            GameObject defaultPickup = Instantiate(defaultPrefab, spawnPosition, Quaternion.identity);
+
+            return defaultPickup;
+        }
+
+        // Handle the case where there's no default prefab set (return null, log an error, or handle it as you prefer)
+        Debug.LogError("No default prefab set for the PickupFactory.");
+        return null;
     }
 
     public void AddPrefab(string type, GameObject prefab)
